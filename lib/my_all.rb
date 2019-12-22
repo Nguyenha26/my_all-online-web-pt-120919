5053541977436fa030391a88e8d6551aac1760f1
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 03:30:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_returns_values = []
   while i < collection.length 
-  
+  block_returns_values <<
   yield(collection[i])
   i = i + 1 
 end
+
 end
